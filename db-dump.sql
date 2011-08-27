e0b4df990d2dd0ee3851a9384dc74f504cb54be7
Carlos Gant <carlos@aicor.com>
2011-08-27 04:12:05 +0200

    Database dump

--- db-dump.sql
+++ db-dump.sql
@@ -16,6 +16,36 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `wiki_menus`
+--
+
+DROP TABLE IF EXISTS `wiki_menus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wiki_menus` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `pages_id` varchar(255) NOT NULL,
+  `order` int(11) NOT NULL,
+  `visible` int(11) NOT NULL,
+  `class` varchar(32) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wiki_menus`
+--
+
+LOCK TABLES `wiki_menus` WRITE;
+/*!40000 ALTER TABLE `wiki_menus` DISABLE KEYS */;
+INSERT INTO `wiki_menus` VALUES (1,'Front Page','Front',1,1,'normal','2011-08-27 01:27:32','0000-00-00 00:00:00'),(2,'Other menu option','OtherMenu2',2,1,'normal','2011-08-27 02:40:32','0000-00-00 00:00:00'),(3,'Menu it\'s editable','OtherMenu3',3,1,'normal','2011-08-27 03:26:48','0000-00-00 00:00:00');
+/*!40000 ALTER TABLE `wiki_menus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `wiki_pages`
 --
 
@@ -43,7 +73,7 @@ CREATE TABLE `wiki_pages` (
 
 LOCK TABLES `wiki_pages` WRITE;
 /*!40000 ALTER TABLE `wiki_pages` DISABLE KEYS */;
-INSERT INTO `wiki_pages` VALUES ('Front','Front Page','Welcome to the CakePHP Wiki!\r\n\r\n* [Testing]',43,'',0,1,'2011-08-25 20:49:48','2011-08-26 00:01:46',1),('Help','Help page','Testing',7,'',0,1,'2011-08-25 21:06:50','2011-08-26 00:00:04',1),('Testing','asd asd','EDUCACIÃ“N FUNDAMENTAL\r\nPSICOLOGÃA REVOLUCIONARIA\r\n       \r\n',61,'',4,0,'2011-08-26 00:05:10','2011-08-26 00:05:10',0);
+INSERT INTO `wiki_pages` VALUES ('Front','Front Page','## Welcome to the CakePHP Wiki!!\r\n\r\n* [Testing]\r\n\r\n<-- Menu here -->',68,'',6,1,'2011-08-25 20:49:48','2011-08-26 00:01:46',1),('Help','Help page','Testing',7,'',0,1,'2011-08-25 21:06:50','2011-08-26 00:00:04',1);
 /*!40000 ALTER TABLE `wiki_pages` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -56,4 +86,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-08-27  0:12:18
+-- Dump completed on 2011-08-27  4:11:56
